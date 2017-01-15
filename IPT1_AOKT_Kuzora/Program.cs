faad1bd09e0df9b950df9eedc08437eb162dde0d
Eugeku <kuzora-home@list.ru>
2017-01-15 18:11:14 +0300

    Добавьте файлы проекта.

---
+++ IPT1_AOKT_Kuzora/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPT1_AOKT_Kuzora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
